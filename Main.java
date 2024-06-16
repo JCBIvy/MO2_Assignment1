56b10d2a8fdf3195a28864992f80cb3ffc14945e
JCBIvy <157339653+JCBIvy@users.noreply.github.com>
2024-06-16 12:27:37 +0000

    Almost Done

--- Main.java
+++ Main.java
@@ -1,16 +1,85 @@
-class Main
-{
-    //Establish constants
+//imports
+import java.util.Scanner;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
-    //INSERT METHOD DESCRIPTION HERE
-    public static String FUNCTION(int ARG)
+class MyDate {
+
+    // fields
+    int year;
+    int month;
+    int day;
+    int speed;
+
+    //noArgs constructor
+    MyDate()
     {
-        return("Blah");
+        GregorianCalendar dateFinder = new GregorianCalendar();
+        this.year = dateFinder.get(GregorianCalendar.YEAR);
+        this.month = dateFinder.get(GregorianCalendar.MONTH);
+        this.day = dateFinder.get(GregorianCalendar.DAY_OF_MONTH);
     }
 
-    //Main body program
+    //elapsed time constructor
+    MyDate(long timeElapsed)
+    {
+        GregorianCalendar dateFinder = new GregorianCalendar();
+        dateFinder.setTimeInMillis(timeElapsed);
+        this.year = dateFinder.get(GregorianCalendar.YEAR);
+        this.month = dateFinder.get(GregorianCalendar.MONTH);
+        this.day = dateFinder.get(GregorianCalendar.DAY_OF_MONTH);
+    }
+    
+    //specified date constructor
+    MyDate(int year, int month, int day)
+    {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    //reset date with elapsed time
+    public void setDate(long timeElapsed)
+    {
+        GregorianCalendar dateFinder = new GregorianCalendar();
+        dateFinder.setTimeInMillis(timeElapsed);
+        this.year = dateFinder.get(GregorianCalendar.YEAR);
+        this.month = dateFinder.get(GregorianCalendar.MONTH);
+        this.day = dateFinder.get(GregorianCalendar.DAY_OF_MONTH);
+    }
+
+    //methods
+    int getYear()
+    {
+        return this.year;
+    }
+
+    int getMonth()
+    {
+        return this.month;
+    }
+
+    int getDay()
+    {
+        return this.day;
+    }
+}
+
+class Main
+{
+    //test out the class
     public static void main(String []args)
     {
-        System.out.println("blah");
+        //test noArgs constructor
+        MyDate date1 = new MyDate();
+        System.out.println(date1.getYear());
+        System.out.println(date1.getMonth());
+        System.out.println(date1.getDay());
+        
+        //test time elapsed constructor
+        MyDate date2 = new MyDate(34355555133101L);
+        System.out.println(date2.getYear());
+        System.out.println(date2.getMonth());
+        System.out.println(date2.getDay());
     }
 };
